436fc8b4b54222a1a5a126c88d80fd4b733ba2ed
ReySebastien <74655004+ReySebastien@users.noreply.github.com>
2021-04-14 09:39:21 +0200

    Update Monstre.h

--- Monstre.h
+++ Monstre.h
@@ -7,33 +7,18 @@ class Monstre{
 
     private:
 
-
-
-
+        int _pvMonstre;
+        int _attaqueMonstre;
+        bool _fatigueMonstre;
 
     public:
 
-
-
-
-
-
+        Monstre();
+        void afficheMonstre();
+        int getPV();
+        int getAttaque();
+        bool getFatigue();
+        bool testVictoireMonstre();
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #endif
