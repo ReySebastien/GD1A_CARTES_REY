f30dd8dbdb2661a1a95ecf6f73e9e170d2727a94
ReySebastien <74655004+ReySebastien@users.noreply.github.com>
2021-04-14 10:36:35 +0200

    création mage.h

---
+++ Mage.h
@@ -0,0 +1,39 @@
+#ifndef MAGE_H
+#define MAGE_H
+
+#include <string>
+
+
+class Mage{
+
+    private:
+
+    std:: string _nomMage;
+    int _pvMage;
+    void _zoneDeJeu;
+
+    public:
+
+    Mage();
+    void afficheMage();
+    void afficheZoneDeJeu(_zoneDeJeu);
+    void ajoutMonstre();
+    void attaqueAdversaire();
+    bool TestVictoireMage();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
